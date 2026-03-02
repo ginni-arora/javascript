a1c5eadf50efe5bd0bb66bf3254261fe97eba227
Ginni Arora <ginnia934@gmail.com>
2026-03-02 19:37:52 +0530

    conversion

---
+++ conversionoperation/conversion.js
@@ -0,0 +1,32 @@
+//let score = 34
+//let score ="34"
+//let score = "34abc"
+//let score = null
+//let score = undefined
+let score = true
+console.log(typeof score)
+console.log(typeof (score))
+let valueinNumber = Number(score)
+console.log(typeof valueinNumber)
+console.log(valueinNumber)
+//"33" convert 33 
+//33abc = NaN = not a number 
+//true = 1 , false = 0
+//let loggedin = 1
+//let loggedin = 0
+//let loggedin = ""
+let loggedin = "ginni"
+
+
+
+
+let booleanloggedin = Boolean(loggedin)
+console.log(booleanloggedin)
+// 1 dala true aya - 0 dalaa to false aya 
+// empty string dala to 0 aya 
+// string m value dali to true aya 
+
+let somenumber = 34
+let stringnumber = String(somenumber)
+console.log(stringnumber)
+console.log(typeof stringnumber)
