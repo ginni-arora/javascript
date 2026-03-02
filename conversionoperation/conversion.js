dce3a52fabd94863f5e483f3a28b76a47640fb19
Ginni Arora <ginnia934@gmail.com>
2026-03-02 19:59:43 +0530

    operation

--- conversionoperation/conversion.js
+++ conversionoperation/conversion.js
@@ -30,3 +30,44 @@ let somenumber = 34
 let stringnumber = String(somenumber)
 console.log(stringnumber)
 console.log(typeof stringnumber)
+
+
+////operations 
+let value = 4
+let negvalue = -value
+console.log(negvalue)
+console.log(2+2)
+console.log(2-2)
+console.log(2*2)
+console.log(2/3)
+console.log(2**3)// 2 tp the power 3
+console.log(2%3)
+// simple very basic operations
+// how to add strings
+let st1 ="helo"
+let st2 = " ginni"
+let st3 = st1+ st2
+console.log(st3)
+//some complex conversions
+console.log("1" + 2)// 12
+console.log("1" + 2 +2 ) //122
+console.log(2+2+ "1") // 41
+// the reason is in documentation when we first add string it trets it as a string 
+//when  the string is in last then it follows the other calculatiins like 41 example
+console.log((3+4)* 5 % 2)
+
+//so we should write our code in parenthesis if we are doing this type of calculations
+
+console.log(+true) //1 ans but we dont need to write this 
+console.log(+"") //0 but this is tricky and we should avoid this 
+
+//avoid writing this type of code ex 
+let num1, num2, num3
+num1 = num2 = num3 = 2+2
+console.log(num1, num2, num3)
+// we should make the code readable not like this
+
+let gamecount = 150 
+//gamecount++ post increment apply krne se pehle value print hogi
+++gamecount//preincrement apply krne se pehle value badh jayegi
+console.log(gamecount)
