7e295d41e29f3863f1dc709d2aa695f5f34a9c96
Ginni Arora <ginnia934@gmail.com>
2026-03-02 19:09:46 +0530

    datatypes

---
+++ variables/datatypes/datatypes.js
@@ -0,0 +1,30 @@
+"use strict";    //treat all js code as modern js code
+
+//alert(3+3 )  because we using node js not browser
+
+//code readibility should be priority
+let name =" ginni"
+let age = 21
+let islogged = false
+let state = null
+let city
+console.table([name, age, islogged, state, city])
+
+//number = 2 to power 53
+//big int = bdi value ka use kriyo which used for big sites like stock etc
+//string = double quote ya single quote 
+//boolean = true or false
+//numm = standalone value 
+//undefined = ki value abhi tk di nahi gyi h 
+//symbol = unique values
+// primitive data types
+
+
+//object 
+
+console.log(typeof "ginni")
+console.log(typeof age)
+console.log(typeof null)
+//lekin jb m null ka type of dekhti hu to vo show krta h ki null ek object hai 
+
+
