e86b9a2c55f475f1419c8dbd47ac7231d2119daf
Ginni Arora <ginnia934@gmail.com>
2026-03-02 18:41:11 +0530

    first commit

---
+++ test.js
@@ -0,0 +1,14 @@
+const accountid = 1442
+let accemail = "ginnia934@gmail.com"
+var accpass= "1234"
+acccity ="haryana"
+
+//accountid = 2  not allowed
+accemail ="ginnia@gmail.com"
+acccity="panipat"
+console.log(accountid)
+console.table([accountid,accemail,accpass, acccity])
+
+/*
+prefer not to use var cause of issue in block scope and functional scope 
+*/
