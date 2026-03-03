5c0e00081fc3ecafb33345e6942add35a50c6e65
Ginni Arora <ginnia934@gmail.com>
2026-03-03 10:49:09 +0530

    memories

---
+++ memories/memories.js
@@ -0,0 +1,23 @@
+// we have 2 types of memories - 
+// stack and heap memory 
+//stack memory is used to store primitive data types and heap memory is used to store non primitive data types
+
+// jab hum stack m store krte h to hume ek copy milti h 
+// or jab hum heao m store krte h to hume reference milta h
+
+
+let a = 10
+let b = a //copy of a is stored in b
+b =5 
+console.log(a) //10
+console.log(b) //5
+
+let user1 = {
+    name: "ginni",
+    age: 22
+}
+let user2 = user1 //reference of user1 is stored in user2
+
+user2.name="ginni arora"
+console.log(user1.name) //ginni arora
+console.log(user2.name) //ginni arora
